046994a81afac3f7d53513859b8227015b1c4433
aakash gupta <aakashgupta2001.2909@gmail.com>
2021-09-25 23:51:22 +0530

    cors

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controller/auth");
 const auth = require("../middlewares/auth");
+const cors = require("cors");
 
-router.route("/signup").post(authController.signup);
+router.route("/signup").post(cors(), authController.signup);
 router.route("/login").post(authController.login);
 
 router.route("/dashboard").post(auth.verifyToken, authController.dashboard);
