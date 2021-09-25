abbec5859543ab7183221e30dc1672ed480d9151
aakash gupta <aakashgupta2001.2909@gmail.com>
2021-09-25 23:20:42 +0530

    added body parser

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 require("./routes").default(app);
 dotenv.config();
-const cors = require("cors");
+
 app.use(
   cors({
     methods: "GET, POST",
     allowedHeaders: "*",
   })
 );
+app.use(bodyParser.json());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.setHeader("Access-Control-Allow-Headers", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   next();
 });
+app.use(express.json());
 
 mongoose.connect(
   process.env.MONGO_URL,
