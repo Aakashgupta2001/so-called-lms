bf42c566fc0c621bf1057e6ac8037d89a87bf606
aakash gupta <aakashgupta2001.2909@gmail.com>
2021-09-26 00:05:40 +0530

    cors

--- index.js
+++ index.js
@@ -16,12 +16,7 @@ app.use(
   })
 );
 app.use(bodyParser.json());
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  next();
-});
+
 app.use(express.json());
 
 mongoose.connect(
